ba680c07ba2d919a43a1d3e93ad0e335099f0b1d
Mahadeer Mohamed <mahadeer2010@gmail.com>
2019-01-01 16:56:29 +0530

    Factories Example Completed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-var ffiBuilder = require("ffi-builder");
+var ffiProxy = require("ffi-proxy");
 
-ffiBuilder
-    .build()
-    .then(console.log)
+ffiProxy
+    .buildAndProxy()
+    .then(function(modules) {
+        var simpleRust = modules.SimpleRust;
+        console.log("(Rust) Fibonacci of 6 is ", simpleRust.fibonacci(6));
+        console.log("(C) DoubleMe of 6 is ", modules.DoubleMe.doubleVal(6));
+        console.log("(C) Addition of 6 and 5 is ", modules["c_libs"].add(6, 5));
+    })
     .catch(console.error);
